4d6d3c0156fca92c5f7f803939445d56eb799b19
Rahul Kumar <rahul.fate0@gmail.com>
2024-02-05 02:10:37 +0530

    final commit

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull rahulfate20/pj1
+docker pull rahulfate20/my-image
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 rahulfate20/pj1
+docker run -d -p 5000:5000 rahulfate20/my-image
